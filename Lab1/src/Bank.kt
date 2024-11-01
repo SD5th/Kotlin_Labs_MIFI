9cd415d7a8c9fe1b9c9d8c213bd7003410dd8894
SD5th <vlaka@ro.ru>
2024-11-02 02:14:33 +0300

    Bank.kt is ready, wasnt tested yet

--- Lab1/src/Bank.kt
+++ Lab1/src/Bank.kt
@@ -13,70 +13,64 @@ class Logger (filePath: String = "/logs/Log.txt") {
     try {
       logFile.appendText("$message\n")
     } catch (e: IOException) {
-      println("Ошибка при записи в лог: ${e.message}")
+      println("Error with writing in log: ${e.message}")
     }
   }
 }
 
-class Bank(
-  private val name: String = "unknown",
-  private var rubBalance: Double = 0.0,
-  private var usdBalance: Double = 0.0,
-  internal var exchangeRate: Double = 1.0
-) {
-  private var logger: Logger = Logger("/logs/Bank_log.txt")
-  private var registerCodes: MutableList<Int> = mutableListOf()
+interface BankInterface {
+  fun createCashRegister(): CashRegister
+}
 
-  internal fun sendRub(amount: Double, registerCode: Int): Double {
-    if (!registerCodes.contains(registerCode)) {
-      throw Exception("Bank '$name': Unknown RegisterCode $registerCode")
-    }
+class Bank(
+  internal val name: String = "unknown",
+  internal var exchangeRate: Double = 1.0,
+  private var rubBalance: Double = 0.0,
+  private var usdBalance: Double = 0.0
+) : BankInterface {
+  private var logger: Logger = Logger("/logs/Bank_log.txt")
+
+  override fun createCashRegister(): CashRegister {
+    logger.log("Bank '$name': Created CashRegister.")
+    return CashRegister(bank = this)
+  }
+
+  internal fun sendRub(amount: Double): Double {
     var toSend = amount
     if (rubBalance < toSend)
       toSend = rubBalance
     rubBalance -= toSend
     logger.log("Bank '$name': Send $toSend roubles.")
+    recalculateExchangeRate()
     return toSend
   }
 
-  internal fun sendUsd(amount: Double, registerCode: Int): Double {
-    if (!registerCodes.contains(registerCode)) {
-      throw Exception("Bank '$name': Unknown RegisterCode $registerCode")
-    }
+  internal fun sendUsd(amount: Double): Double {
     var toSend = amount
     if (usdBalance < toSend)
       toSend = usdBalance
     usdBalance -= toSend
     logger.log("Bank '$name': Send $toSend baksov.")
+    recalculateExchangeRate()
     return toSend
   }
 
-  internal fun receiveRub(amount: Double, registerCode: Int) {
-    if (!registerCodes.contains(registerCode)) {
-      throw Exception("Bank '$name': Unknown RegisterCode $registerCode")
-    }
+  internal fun receiveRub(amount: Double) {
     rubBalance += amount
     logger.log("Bank '$name': Received $amount rubs.")
+    recalculateExchangeRate()
   }
 
-  internal fun receiveUsd(amount: Double, registerCode: Int) {
-    if (!registerCodes.contains(registerCode)) {
-      throw Exception("Bank '$name': Unknown RegisterCode $registerCode")
-    }
+  internal fun receiveUsd(amount: Double) {
     usdBalance += amount
     logger.log("Bank '$name': Received $amount baksov.")
+    recalculateExchangeRate()
   }
 
-  fun createCashRegister(): CashRegister {
-    var registerCode = Random.nextInt()
-    while (!registerCodes.contains(registerCode)) {
-      registerCode = Random.nextInt()
-    }
-    registerCodes.addLast(registerCode)
-    logger.log("Bank '$name': Created CashRegister with code: $registerCode")
-    return CashRegister(bank = this, code = registerCode)
+  private fun recalculateExchangeRate() {
+    exchangeRate *= Random.nextDouble(0.5, 2.0)
+    logger.log("Bank '$name': Recalculated ExchangeRate. Currently: $exchangeRate.")
   }
-
 }
 
 interface CashRegisterInterface {
@@ -85,23 +79,30 @@ interface CashRegisterInterface {
   fun checkExchangeRate(): Double
 }
 
-class CashRegister (private var bank: Bank, private val code: Int) : CashRegisterInterface
+class CashRegister internal constructor(private var bank: Bank) : CashRegisterInterface
 {
+  private var exchangeRate: Double = bank.exchangeRate
+  private var logger: Logger = Logger("/logs/CashRegister_log.txt")
 
   override fun exchangeRubToUsd(rubAmount: Double): Double {
-    val usdAmount = bank.sendUsd(rubAmount * bank.exchangeRate, code)
-    bank.receiveRub(rubAmount, code)
+    val usdAmount = bank.sendUsd(rubAmount * bank.exchangeRate)
+    bank.receiveRub(rubAmount)
+    exchangeRate = bank.exchangeRate
+    logger.log("Bank '${bank.name}': Exchanged $rubAmount of roubles for $usdAmount of baksov.")
     return usdAmount
   }
 
   override fun exchangeUsdToRub(usdAmount: Double): Double{
-    val rubAmount = bank.sendRub(usdAmount * (1 / bank.exchangeRate), code)
-    bank.receiveUsd(usdAmount, code)
+    val rubAmount = bank.sendRub(usdAmount * (1 / bank.exchangeRate))
+    bank.receiveUsd(usdAmount)
+    logger.log("Bank '${bank.name}': Exchanged $usdAmount of baksov for $rubAmount of roubles.")
+    exchangeRate = bank.exchangeRate
     return rubAmount
   }
 
   override fun checkExchangeRate(): Double {
-    return bank.exchangeRate
+    logger.log("Bank '${bank.name}': Checked ExchangeRate. Currently: $exchangeRate.")
+    return exchangeRate
   }
 }
 
